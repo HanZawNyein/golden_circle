42f211e075ed7ea8e00884954f2589df97b69519
Agga <agga@Aggas-MacBook-Air.local>
2024-05-18 11:11:54 +0630

    [add] mutation

--- auth/crud.py
+++ auth/crud.py
@@ -29,3 +29,34 @@ async def authenticate_user(db: AsyncSession, username: str, password: str):
     if not user or not verify_password(password, user.hashed_password):
         return False
     return user
+
+
+async def change_password(db: AsyncSession, user_id: int, new_password: str)->bool:
+    user = await get_user_by_id(db, user_id)
+    if user:
+        user.hashed_password = get_password_hash(new_password)
+        await db.commit()
+        return True
+    return False
+
+
+async def request_password_reset(db: AsyncSession, username: str)->bool:
+    user = await get_user_by_username(db, username)
+    if user:
+        # Implement logic to send password reset email
+        # For example, generate a unique token and send it to the user's email
+        # Then update the user's database record with the token
+        # user.reset_token = generate_reset_token()
+        # await db.commit()
+        return True
+    return False
+
+
+async def reset_password(db: AsyncSession, username: str, reset_token: str, new_password: str)->bool:
+    user = await get_user_by_username(db, username)
+    if user and user.reset_token == reset_token:
+        user.hashed_password = get_password_hash(new_password)
+        user.reset_token = None  # Clear the reset token
+        await db.commit()
+        return True
+    return False
--- auth/mutation.py
+++ auth/mutation.py
@@ -4,10 +4,12 @@ from strawberry.types import Info
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from graphql_services.context import get_context, Context
-from auth.crud import create_user, authenticate_user
-from auth.token import create_access_token
+from auth.crud import create_user, authenticate_user, get_user_by_username, request_password_reset, reset_password, \
+    change_password
+from auth.token import create_access_token, verify_password
 
-from .types import User,Token
+from .types import User, Token
+
 
 # Define GraphQL mutations
 @strawberry.type
@@ -26,3 +28,32 @@ class AuthenticationMutation:
             raise HTTPException(status_code=400, detail="Invalid credentials")
         access_token = create_access_token(data={"sub": str(user.id)})
         return Token(access_token=access_token, token_type="bearer")
+
+    @strawberry.mutation
+    async def changePassword(self, new_password: str, info: strawberry.Info[Context]) -> str:
+        db: AsyncSession = info.context.db
+        user_id = info.context.user
+        user = await change_password(db, user_id, new_password)
+        print(user,user_id,db)
+        if user:
+            return "Password Change Successfully."
+        else:
+            raise ValueError("User not found")
+
+    @strawberry.mutation
+    async def requestPasswordReset(self, username: str, info: strawberry.Info[Context]) -> str:
+        db: AsyncSession = info.context.db
+        user = request_password_reset(db, username)
+        if user:
+            return "Password reset email sent"
+        else:
+            raise ValueError("User not found")
+
+    @strawberry.mutation
+    async def resetPassword(self, username: str, info: strawberry.Info[Context]) -> str:
+        db: AsyncSession = info.context.db
+        user = reset_password(db, username, reset_token='112', new_password='122')
+        if user:
+            return "Password reset email sent"
+        else:
+            raise ValueError("User not found")
