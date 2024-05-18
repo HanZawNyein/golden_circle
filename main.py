da93482966310cd6937008c9fc60e1679459bc45
Agga <agga@Aggas-MacBook-Air.local>
2024-05-18 09:02:26 +0630

    [warning] custom middleware in type

--- main.py
+++ main.py
@@ -2,10 +2,11 @@ from schema import graphql_app
 from fastapi import FastAPI
 from database.middleware import AddDbToRequestMiddleware
 from database.events import start_db
+from typing import Type
 
 app = FastAPI()
 
-app.add_middleware(AddDbToRequestMiddleware)
+app.add_middleware(Type[AddDbToRequestMiddleware])
 app.add_event_handler("startup", start_db)
 
 
