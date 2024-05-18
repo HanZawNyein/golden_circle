08693afb28e0896aebbf99492aa0e33bbc30d77d
Agga <agga@Aggas-MacBook-Air.local>
2024-05-18 11:34:56 +0630

    [fix] debugging

--- graphql_services/app.py
+++ graphql_services/app.py
@@ -1,9 +1,10 @@
+import os
+
 from strawberry.fastapi import GraphQLRouter
 from strawberry.schema import Schema
 from .context import get_context
 from .query import Query
 from .mutation import Mutation
-
 # Create GraphQL schema
 schema = Schema(query=Query, mutation=Mutation)
 graphql_app = GraphQLRouter(schema, context_getter=get_context, debug=True)
