74be3cc45989ace4ff732cf5bae4ba78a47ee36c
Agga <agga@Aggas-MacBook-Air.local>
2024-05-19 14:53:07 +0630

    [fix] todo mutation

--- goldenCircle/graphql_services/app.py
+++ goldenCircle/graphql_services/app.py
@@ -1,6 +1,7 @@
 from strawberry.fastapi import GraphQLRouter
 from strawberry.schema import Schema
 
+from todo.todo_mutation import TodoMutation
 from .context import get_context
 from .mutation import Mutation
 from .query import Query
--- main.py
+++ main.py
@@ -4,6 +4,10 @@ from goldenCircle.database.events import start_db
 from goldenCircle.database.middleware import AddDbToRequestMiddleware
 from goldenCircle.graphql_services import graphql_app
 
+# Replace the mutation schema with the updated Mutation class
+# graphql_app.schema.mutation.todo = strawberry.mutation(lambda: TodoMutation())
+# graphql_app.schema.query.todo: TodoQuery = strawberry.field(lambda: TodoQuery())
+
 app = FastAPI()
 
 app.add_middleware(AddDbToRequestMiddleware)
